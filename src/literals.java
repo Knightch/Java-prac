5c81e1bb90a055c9eb067ecf123fdb43c13c2227
knightch <amankumarpandey2397@gmail.com>
2022-06-30 19:35:49 +0530

    literals

---
+++ src/literals.java
@@ -0,0 +1,14 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age = 40;
+        char ch = 'A';
+        float num = 23.34f;
+        double dub = 234.4578d;
+        long longVal = 1438787999576898798L;
+        boolean tripOn = false;
+        String name = "Aman";
+        System.out.println(age);
+        System.out.println(num);
+        System.out.println(name);
+    }
+}
