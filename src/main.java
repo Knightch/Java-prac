f538fbf85dde6f7c8264480850bab0cd47d34686
knightch <amankumarpandey2397@gmail.com>
2022-06-30 12:35:59 +0530

    adding two number

--- src/main.java
+++ src/main.java
@@ -1,6 +1,11 @@
 
 public  class main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.print("sum of three number is ");
+        int num1 = 6;
+        int num2 = 8;
+        int num3 = 9;
+        int sum = num1+num2+num3;
+        System.out.print(sum);
     }
 }
