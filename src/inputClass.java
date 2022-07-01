210bf13b8783f83ceb28455b85a64dfaca2e9e6a
knightch <amankumarpandey2397@gmail.com>
2022-07-01 21:43:01 +0530

    sum of decimal number enter by users

--- src/inputClass.java
+++ src/inputClass.java
@@ -3,11 +3,17 @@ public class inputClass {
     public static void main(String[] args) {
         System.out.println("Taking Input from user");
         Scanner newInput = new Scanner(System.in);
-        System.out.println("Enter a number ");
-        int num1 = newInput.nextInt();
-        System.out.println("Enter a another number");
-        int num2 = newInput.nextInt();
-        int sum = num1 + num2 ;
-        System.out.println("Sum of given number is "+ sum);
+//        System.out.println("Enter a number ");
+//        int num1 = newInput.nextInt();
+        System.out.println("Enter a decimal number");
+        float num1 = newInput.nextFloat();
+//        System.out.println("Enter a another number");
+        System.out.println("Enter a another decimal number");
+//        int num2 = newInput.nextInt();
+        float num2 = newInput.nextFloat();
+//        int sum = num1 + num2 ;
+        float sum = num1 + num2;
+//        System.out.println("Sum of given number is "+ sum);
+        System.out.println("Sum of given decimal number is "+ sum);
     }
 }
