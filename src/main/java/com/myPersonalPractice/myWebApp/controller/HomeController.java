8647459a2ad39b0e0860c9c1c20c01049070db76
knightch <amankumarpandey2397@gmail.com>
2023-02-01 16:54:24 +0530

    using application.properties for config

--- src/main/java/com/myPersonalPractice/myWebApp/controller/HomeController.java
+++ src/main/java/com/myPersonalPractice/myWebApp/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.myPersonalPractice.myWebApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,6 +10,6 @@ public class HomeController {
     @RequestMapping("home")
     public String home(){
         System.out.println("welcome to home!");
-        return "home.html";
+        return "home";
     }
 }
