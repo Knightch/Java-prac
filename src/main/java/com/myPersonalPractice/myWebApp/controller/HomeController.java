cf1c2920dad96cf71a28b4f9b3d927ab2275cd1e
knightch <amankumarpandey2397@gmail.com>
2023-02-01 18:17:30 +0530

    using model and view for sending data and view the data!

--- src/main/java/com/myPersonalPractice/myWebApp/controller/HomeController.java
+++ src/main/java/com/myPersonalPractice/myWebApp/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.myPersonalPractice.myWebApp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(){
-        System.out.println("welcome to home!");
-        return "home";
+    public ModelAndView home(@RequestParam("name") String myName){
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name",myName);
+        mv.setViewName("home");
+        System.out.println("welcome to home! " + myName);
+
+        return mv;
     }
 }
